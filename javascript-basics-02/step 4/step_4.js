2b2b55e8451198f25fb041e4414a1b9555f87de0
rebecca <rebeccanazha07@agmail.com>
2020-10-06 16:31:50 +0300

    basic_2step.4

---
+++ javascript-basics-02/step 4/step_4.js
@@ -0,0 +1,15 @@
+let button = document.getElementsByTagName('button');
+
+
+button = document.addEventListener('click', function () {
+    let inputName = document.getElementById('name');
+    let inputSurname = document.getElementById('surname');
+    let inputCity = document.getElementById('city');
+    inputName.value ='';
+    inputSurname.value ='';
+    inputCity.value ='';
+
+
+  });
+
+        
