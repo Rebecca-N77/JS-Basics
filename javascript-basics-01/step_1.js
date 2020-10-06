0a84d8f42ce008cb44f473e3ec61e544c7eee77c
rebecca <rebeccanazha07@agmail.com>
2020-10-06 14:32:29 +0300

    basic_1.step_1+2

---
+++ javascript-basics-01/step_1.js
@@ -0,0 +1,2 @@
+let name = 'Rebecca';
+alert(name)
---
+++ javascript-basics-01/step_2.js
@@ -0,0 +1,4 @@
+let name = 'Rebecca';
+let surName ='Nazha';
+let city = 'Beirut';
+alert ('Name:  ' + name +  '\n' + 'surName :  ' + surName + '\n' +'city:  ' + city);
