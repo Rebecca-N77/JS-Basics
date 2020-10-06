3317ab6e2f1e0b61823878e923b656e66d3a8eaf
rebecca <rebeccanazha07@agmail.com>
2020-10-06 14:43:01 +0300

    basic_1step_4

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,4 @@
+let name = window.prompt('Please enter you name');
+let surName= window.prompt('Please enter your surname');
+let city=window.prompt('Please enter your city');
+alert('Hello '+ name + " " + surName +' from '+city );
